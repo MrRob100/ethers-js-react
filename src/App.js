5d8e3392f5c6972028d09ca4645236dd810f3e66
Rob A <robarnold100@hotmail.com>
2022-06-11 18:13:22 -0300

    greeting reading

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ function App() {
   const greeting = 'none';
   const infuraKey = process.env.REACT_APP_INFURA_KEY;
 
-  const provider = new providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${infuraKey}`)
+  const provider = new providers.JsonRpcProvider(`https://ropsten.infura.io/v3/${infuraKey}`)
+
+  const contractAddress = process.env.REACT_APP_SMART_CONTRACT_ADDRESS;
+  const abi = '[{"inputs":[],"name":"Greet","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_greeting","type":"string"}],"name":"setGreeting","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"greet","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"greeting","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"sayHey","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"what","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}]';
+
+  const contract = new Contract(contractAddress, abi, provider);
 
   const main = async () => {
     const balance = await provider.getBalance(process.env.REACT_APP_ETH_METAMASK_ADDRESS);
-    console.log(utils.formatEther(balance));
+    console.log('balance', utils.formatEther(balance));
+
+    const greeting2 = await contract.greet()
+    console.log('greeting', greeting2); 
   }
 
   main()
