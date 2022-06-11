5823e367754384f8cee6adb7feac38ce47f0e2d5
Rob A <robarnold100@hotmail.com>
2022-06-11 11:32:24 -0300

    contract connection using ethers js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import Web3 from 'web3';
-// import {useEffect} from "react";
+
+import { getDefaultProvider, providers, Wallet, Contract } from "ethers";
+
+import {useEffect, useState} from "react";
 
 function App() {
-  // const providerUrl = process.env.PROVIDER_URL;
-  // const infuraKey = process.env.INFURA_KEY;
-  //
-  // const web3 = new Web3(providerUrl + infuraKey);
+  const [greeting, setGreeting] = useState('empty');
+  const providerUrl = process.env.REACT_APP_PROVIDER_URL;
+  const infuraKey = process.env.REACT_APP_INFURA_KEY;
+  const contractAddress = process.env.REACT_APP_SMART_CONTRACT_ADDRESS;
 
-  // const contractAddress = process.env.SMART_CONTRACT_ADDRESS;
-  // const abi = JSON.parse('[{"inputs": [],"name": "Greet","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "string","name": "_greeting","type": "string"}],"name": "setGreeting","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "greet","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "greeting","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"}]');
-  //
-  // const myContract = new web3.eth.Contract(abi, contractAddress);
-  // // const myContract = web3.eth.Contract(abi, contractAddress);
-  //
-  // myContract.methods.greet().call().then( function( info ) {
-  //   console.log("info: ", info);
-  // });
+  const provider = new getDefaultProvider("ropsten", {
+    infura: "b59953df17ce4e248a1198806fe9c4bd",
+  });
 
+  const signer = Wallet.createRandom();
+
+  const abi = '[{ "inputs": [], "name": "Greet", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "_greeting", "type": "string" }], "name": "setGreeting", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "greet", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "greeting", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }]'
+
+  const contract = new Contract(contractAddress, abi, provider)
+
+  console.log('contract', contract);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Greeting: {greeting}</h3>
     </div>
   );
 }
